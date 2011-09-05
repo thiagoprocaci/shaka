11de10717cd64c754456d0d8d7db62446439ab8c
thiagoprocaci <thiagoprocaci@gmail.com>
2011-09-05 17:32:37 -0300

    testes

--- test/unit/com/shaka/controller/UsuarioControllerTests.groovy
+++ test/unit/com/shaka/controller/UsuarioControllerTests.groovy
@@ -2,22 +2,48 @@ package com.shaka.controller
 
 import grails.test.*
 
+import com.shaka.Usuario
 import com.shaka.UsuarioController
+import com.shaka.business.UsuarioService;
 
 class UsuarioControllerTests extends ControllerUnitTestCase {
+
+    def usuarioService
 
     public UsuarioControllerTests() {
         super(UsuarioController.class)
     }
     protected void setUp() {
         super.setUp()
+        mockDomain(Usuario)
+        usuarioService = mockFor(UsuarioService)
+        controller.usuarioService = usuarioService.createMock()
     }
 
     protected void tearDown() {
         super.tearDown()
     }
 
-    void testSomething() {
+    void testIndex() {
+        def paramsMock = [id : 1]
+        controller.params.putAll(paramsMock)
+        controller.index()
+        assertNotNull controller.redirectArgs
+        assertNotNull controller.redirectArgs.action
+        assertNotNull controller.redirectArgs.params
+        assertEquals 2, controller.redirectArgs.size()
+        assertEquals "create", controller.redirectArgs.action
+        assertEquals paramsMock, controller.redirectArgs.params
+    }
 
+    void testCreate() {
+        def paramsMock = [nome : "nome"]
+        controller.params.putAll(paramsMock)
+        def map = controller.create()
+        assertNotNull map
+        assertNotNull map.usuarioInstance
+        assertNotNull map.usuarioInstance.nome
+        assertEquals paramsMock.nome, map.usuarioInstance.nome
+        assertEquals 1, map.size()
     }
 }
