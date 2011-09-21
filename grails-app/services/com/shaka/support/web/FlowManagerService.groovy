6b8c485ac28ec1232b5c219dad8555df538656ef
thiagoprocaci <thiagoprocaci@gmail.com>
2011-09-20 23:02:06 -0300

    ajustes no flowManager e mensageria

--- grails-app/services/com/shaka/support/web/FlowManagerService.groovy
+++ grails-app/services/com/shaka/support/web/FlowManagerService.groovy
@@ -9,33 +9,54 @@ import javax.servlet.http.HttpServletRequest
  *
  */
 class FlowManagerService {
-    static scope = 'session'
-    static transactional = false
+	static scope = 'session'
+	static transactional = false
 
-    String lastGetUrl
-    String lastPostUrl
+	String lastGetUrl
+	String lastPostUrl
+	String lastAjaxUrl
 
-    /**
-     * Processa a url vinda do request
-     */
-    def processRequest = { request ->
-        if(request instanceof HttpServletRequest){
+	/**
+	 * Processa a url vinda do request
+	 */
+	def processRequest = {
+		request ->
+		// somente http request...
+		if(request instanceof HttpServletRequest){
 			// nao nos interessa ajax request
-			if(!'XMLHttpRequest'.equals(request.getHeader('X-Requested-With'))){
+			if(isAjax(request) == false){
 				if("POST".equals(request.getMethod())) {
 					lastPostUrl = request.getRequestURL()
 				} else if ("GET".equals(request.getMethod())){
 					lastGetUrl = request.getRequestURL()
 				}
+			} else {
+				lastAjaxUrl = request.getRequestURL()
 			}
-        }
-    }
+		}
+	}
 
-    /**
-     * Retorna ultima url get
-     */
-    def getLastGetUrl = {
-        return lastGetUrl
-    }
+	/**
+	 * Retorna ultima url get
+	 */
+	def getLastGetUrl = {
+		return lastGetUrl
+	}
+
+	private boolean isAjax(def request) {
+		if("XMLHttpRequest".equals(request.getHeader("X-Requested-With"))){
+			return true
+		}
+		// assume-se que as requisicoes ajax contem a string "ajax" na url...
+		// a primeira condicao nao eh suficiente para diferentes ambientes
+		String url = request.getRequestURL()
+		if(url.contains("ajax")){
+			return true
+		}
+		if(url.endsWith(".dispatch")){
+			return true
+		}
+		return  false
+	}
 
 }
